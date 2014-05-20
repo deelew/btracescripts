3b12919a845a2347f7a5aa08c92105e77e000b30
deelew <deelew.kxq@gmail.com>
2014-05-20 18:46:11 +0800

    commit

--- src/main/java/dilu/kxq/btrace/TraceDirectBufferAllocate.java
+++ src/main/java/dilu/kxq/btrace/TraceDirectBufferAllocate.java
@@ -1,11 +1,27 @@
 package dilu.kxq.btrace;
 
+import com.sun.btrace.annotations.BTrace;
+import com.sun.btrace.annotations.OnMethod;
+import static com.sun.btrace.BTraceUtils.*;
 /**
  * Created with IntelliJ IDEA.
  * User: dilu.kxq
  * Date: 14-5-20
- * Time: 下午5:27
  * To change this template use File | Settings | File Templates.
  */
+@BTrace
 public class TraceDirectBufferAllocate {
+    private static int printCount = 0;
+    @OnMethod(clazz = "java.nio.ByteBuffer", method = "allocateDirect")
+    public static void func(int size) {
+        if(printCount++ < 10){
+
+            println("=======stack======");
+            println(concat("bufferSize=", str(size)));
+            jstack();
+            println("########end#########");
+        }
+        else
+            exit(0);
+    }
 }
